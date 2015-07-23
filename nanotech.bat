b700a7d4c08a72430c948a99faa0df5ebebb2f66
Michael <mike-bailey@users.noreply.github.com>
2015-07-23 18:53:18 -0400

    Update nanotech.bat

--- nanotech.bat
+++ nanotech.bat
@@ -12,7 +12,7 @@ del CreateShortcut.vbs
 REM Flash Download, Installation, etc
 cd C:\Windows\Temp\
 echo ' Set your settings > C:\Windows\Temp\downloadAdobe.vbs
-echo    strFileURL = "https://fpdownload.macromedia.com/get/flashplayer/current/licensing/win/install_flash_player_18_active_x.msi" >> C:\Windows\Temp\downloadAdobe.vbs
+echo    strFileURL = "http://fpdownload.macromedia.com/get/flashplayer/current/licensing/win/install_flash_player_18_active_x.msi" >> C:\Windows\Temp\downloadAdobe.vbs
 echo    strHDLocation = "C:\Windows\Temp\flash.msi" >> C:\Windows\Temp\downloadAdobe.vbs
 echo. >> C:\Windows\Temp\downloadAdobe.vbs
 echo ' Fetch the file >> C:\Windows\Temp\downloadAdobe.vbs
@@ -44,7 +44,7 @@ msiexec /i "flash.msi" /qn
 echo Download and Installation done for ActiveX version...
 echo Installing new version...
 echo ' Set your settings > C:\Windows\Temp\downloadAdobe2.vbs
-echo    strFileURL = "https://fpdownload.macromedia.com/get/flashplayer/current/licensing/win/install_flash_player_18_plugin.msi" >> C:\Windows\Temp\downloadAdobe2.vbs
+echo    strFileURL = "http://fpdownload.macromedia.com/get/flashplayer/current/licensing/win/install_flash_player_18_plugin.msi" >> C:\Windows\Temp\downloadAdobe2.vbs
 echo    strHDLocation = "C:\Windows\Temp\flash2.msi" >> C:\Windows\Temp\downloadAdobe2.vbs
 echo. >> C:\Windows\Temp\downloadAdobe2.vbs
 echo ' Fetch the file >> C:\Windows\Temp\downloadAdobe2.vbs
@@ -111,10 +111,10 @@ echo Determining Processor Architecture
 echo ' Set your settings > C:\Windows\Temp\downloadChrome.vbs
 wmic path win32_processor where deviceid="cpu0" get Addresswidth | findstr 32
 if "%ERRORLEVEL%"=="1" goto :64
-echo    strFileURL = "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BC8E80393-F270-CD1B-C64C-3515C3949032%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers/dl/chrome/install/googlechromestandaloneenterprise.msi" >> C:\Windows\Temp\downloadChrome.vbs
+echo    strFileURL = "http://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BC8E80393-F270-CD1B-C64C-3515C3949032%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers/dl/chrome/install/googlechromestandaloneenterprise.msi" >> C:\Windows\Temp\downloadChrome.vbs
 goto :end64
 :64
-echo    strFileURL = "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BC8E80393-F270-CD1B-C64C-3515C3949032%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers/dl/chrome/install/googlechromestandaloneenterprise64.msi" >> C:\Windows\Temp\downloadChrome.vbs
+echo    strFileURL = "http://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BC8E80393-F270-CD1B-C64C-3515C3949032%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers/dl/chrome/install/googlechromestandaloneenterprise64.msi" >> C:\Windows\Temp\downloadChrome.vbs
 :end64
 echo    strHDLocation = "C:\Windows\Temp\chrome.msi" >> C:\Windows\Temp\downloadChrome.vbs
 echo. >> C:\Windows\Temp\downloadChrome.vbs
